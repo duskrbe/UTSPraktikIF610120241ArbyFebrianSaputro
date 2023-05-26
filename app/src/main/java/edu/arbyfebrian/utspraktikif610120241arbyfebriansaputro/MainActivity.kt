2522fb7d1c9e39db488187958b50ffc18bb09bd7
Arby Febrian Saputro <oginkacida2@gmail.com>
2023-05-26 17:52:43 +0700

    komentar

--- app/src/main/java/edu/arbyfebrian/utspraktikif610120241arbyfebriansaputro/MainActivity.kt
+++ app/src/main/java/edu/arbyfebrian/utspraktikif610120241arbyfebriansaputro/MainActivity.kt
@@ -2,7 +2,10 @@ package edu.arbyfebrian.utspraktikif610120241arbyfebriansaputro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//26-5-2023
+//10120241
+//Arby Febrian Saputro
+//IF-6
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
